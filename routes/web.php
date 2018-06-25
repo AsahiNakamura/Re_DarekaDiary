ef68cd504ef4947d8dc30bfa09d40aa8847b7c49
asahi nakamura <0.0.nn.ai@gmail.com>
2018-06-25 19:23:07 +0900

    完成かな (#2)
    * 各ページへのリンクまで
    
    * 登録とかページ遷移とか一通り
    
    * 一部ミスを修正
    
    * タイトルの変更とか
    
    * 過去ページの表示処理
    
    * 諸変更

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,14 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/update', 'Controller@update');
+Route::get('/entry', 'Controller@formAction');
+Route::get('/edit', 'Controller@formAction');
 
 Route::get('/', 'Controller@index');
+Route::get('/{date}', 'Controller@showOldEntry');
