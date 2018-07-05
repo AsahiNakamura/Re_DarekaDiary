96f3808ecfac04c050bf46e15f45c7030bd9dde1
asahi-nakamura <asahi_nakamura@c.dwango.co.jp>
2018-07-05 14:23:54 +0900

    メンテナンス画面追加

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+@extends('template.header')
+@section('title', 'Re: 誰かダイアリー')
+@section('content')
+    <style>
+        span{
+            font-weight: bold;
+        }
+    </style>
+    <h1>Re: 誰かダイアリー</h1>
+    <p>ごめんなさい、メンテナンス中です...</p>
+    
+@endsection
